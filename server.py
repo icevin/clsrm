5bf8d68a1436ed8520068c3a468a1980db39e580
Kevin Tang <kevintang2023@gmail.com>
2020-03-28 06:36:07 -0700

    Cookies

--- server.py
+++ server.py
@@ -1,18 +1,77 @@
-from flask import Flask, request
-from twilio.twiml.messaging_response import MessagingResponse
+from flask import Flask, request, session
+from twilio.twiml.messaging_response import (
+    MessagingResponse,
+    Message,
+    Body,
+    Media
+)
 
+SECRET_KEY = 'LAHACKS2020'
 app = Flask(__name__)
+app.config.from_object(__name__)
+
 
 @app.route("/sms", methods=['GET', 'POST'])
-def sms_ahoy_reply():
-    """Respond to incoming messages with a friendly SMS."""
+def parser():
+
+    # Get status from cookie
+    current_status = session.get('status', '/')
+
+    # TODO: Keep track of what IDs the options I give correspond to
+
+
+    # status syntax:
+    # /ccccc/aaaaa/<state>
+    # ccccc = class id
+    # aaaaa = post or assignment
+    # <state
+
+    # Set up output vars
+    out_status_text = ''
+    out_content_text = ''
+    out_options = []
+
+    new_status = '/'
+
+    # TODO: Verify/identify number
+
+    from_number = request.form['From']
+    from_text = request.form['Body']
+
+
+    path = current_status.split('/')
+
+    if 0:
+        print("false")
+    elif from_text.isdigit():
+        new_status = current_status + from_text + '/'
+        out_status_text += 'Up one level'
+    elif str.lower(from_text) == 'home' or current_status == '/':
+        new_status = '/'
+        out_status_text = 'Home'
+        out_content_text = ''
+        # Add classes options: out_options.append('')
+        for n in range(1, 4):
+            test = str(n) + ') ' + 'Option ' + str(n)
+            out_options.append(test)
+    else:
+        new_status=current_status
+
+    session['status']=new_status
+    
+    out_options_text="\n".join(option for option in out_options)
+    
+    out_text='{}:\n{}\nOptions:\n{}' \
+        .format(out_status_text, out_content_text, out_options_text)
+
     # Start our response
-    resp = MessagingResponse()
+    resp=MessagingResponse()
 
     # Add a message
-    resp.message("Ahoy! Thanks so much for your message.")
+    resp.message(out_text)
 
     return str(resp)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
